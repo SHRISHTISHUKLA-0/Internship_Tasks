9a54eb6fb2c2acf1e708e7f492b9879f9b014599
SHRISHTISHUKLA-0 <shrishtishukla01332@gmail.com>
2025-06-10 23:15:42 +0530

    Added Task 1 - User Authentication

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/Task_1_Auth");
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    post: [{ type: mongoose.Schema.Types.ObjectId, ref: "posts"}],
+});
+
+module.exports = mongoose.model('user', userSchema);
